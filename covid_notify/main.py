1bd7ade9f52beead65bdd12478bc252d23fc64cf
Areeb <areeb.faridy@gmail.com>
2023-03-14 03:10:09 +0530

    logic for scraping data into list format

--- covid_notify/main.py
+++ covid_notify/main.py
@@ -1,5 +1,6 @@
 import subprocess
 import requests
+from bs4 import BeautifulSoup
 CMD = '''
 on run argv
   display notification (item 2 of argv) with title (item 1 of argv) 
@@ -16,6 +17,50 @@ def getData(url):
 # Example uses:
 notify("james bond", "Heres an alert", "INDIA")
 
-myhtmlData=getData("https://www.mohfw.gov.in/")
-print(myhtmlData)
+myhtmlData=getData("https://prsindia.org/covid-19/cases")
+
+
+soup=BeautifulSoup(myhtmlData, 'html.parser')
+
+DataList=[]
+
+for td in soup.find_all('tbody')[0].find_all('tr'):
+  list1=[]
+  count=0
+  for item in td.find_all('td'):
+    list1.append(item.get_text())
+    count+=1
+    if(count==5):
+      print(list1)
+      DataList.append(list1)
+    #   print(DataList)
+      list1=[]
+      count=0
+
+print(len(DataList))
+
+for item in DataList:
+  print(item)
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+        
+  
+
+
+  
+
+
+
 
